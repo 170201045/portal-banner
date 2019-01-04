20234dfc5ffe41356232cef223866d5141c65815
Mert İnal <mertinal1998@hotmail.com>
2019-01-04 18:28:58 +0300

    changed
    changed

--- views/backend/default/index.php
+++ views/backend/default/index.php
@@ -1,12 +1,16 @@
-<?= $this->title = 'Modül çalışıyor.'; ?>
-<div class="site-index">
+<?= $this->title = 'Modül Başarılı'; ?>
+<div class="site-index" style="background-color: grey">
 
     <div class="jumbotron">
-        <h1>Kou Osl Yii2 App</h1>
+        <h1>Banner Modülü</h1>
 
-        <p class="lead">Örnek uygulamayı başarılı bir şekilde çalıştırdınız.</p>
+        <p class="lead">Banner Modülü Başarı İle Oluştu</p>
 
-        <p><a class="btn btn-lg btn-success" href="#">Modüller ve konfürgasyon!</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://portal.kouosl/admin/banner/bannerr/create">Yeni Banner Oluştur</a></a>
+<a class="btn btn-lg btn-primary" href="http://portal.kouosl/admin/banner/bannerr/index">Bannerları Görüntüle</a></a>
+        </p>
+        
+     
     </div>
 
 </div>
