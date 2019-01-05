a1d30b273446abab058d8ccadcf20cb6e78049f9
Mert İnal <mertinal1998@hotmail.com>
2019-01-05 22:17:20 +0300

    Update m190104_013926_banner.php

--- migrations/m190104_013926_banner.php
+++ migrations/m190104_013926_banner.php
@@ -10,7 +10,7 @@ class m190104_013926_banner extends Migration
     /**
      * {@inheritdoc}
      */
-    public function safeUp()
+    public function up()
     {
 
 
@@ -30,25 +30,10 @@ class m190104_013926_banner extends Migration
     /**
      * {@inheritdoc}
      */
-    public function safeDown()
-    {
-        echo "m190104_013926_banner cannot be reverted.\n";
-
-        return false;
-    }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
     public function down()
     {
-        echo "m190104_013926_banner cannot be reverted.\n";
-
-        return false;
+         $this->dropTable('bannerr');
     }
-    */
+
+
 }
